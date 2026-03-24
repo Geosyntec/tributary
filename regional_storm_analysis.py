a67acd3364c1d7e8ffa467dbbbd69e54a3f3d0f9
Guy McFeeters-Krone <guy.mcfeeters@geosyntec.com>
2026-03-24 16:18:45 -0700

    Correct issue with config pulls, create basic framework for storm analysis

--- regional_storm_analysis.py
+++ regional_storm_analysis.py
@@ -1,7 +1,7 @@
 
 from config import OUTPUT_DIR
 from logging_setup import setup_logging, get_logger
-from data_loader import find_latest_database, load_database_to_dataframe, _load_from_database
+from data_loader import find_latest_database, load_rainfall_data, filter_by_date
 
 setup_logging()
 logger = get_logger(__name__)
@@ -11,16 +11,17 @@ def main():
     logger.info("Starting gauge analysis...")
     # Find the database
     db_path = find_latest_database(OUTPUT_DIR)
-    print(f"Using database: {db_path}")
+    logger.info(f"Using database: {db_path}")
 
     if db_path is None:
         logger.error("Cannot continue without a database file")
         return
     
     # Load into DataFrame
-    rain_df = load_database_to_dataframe(db_path)
+    rain_df = load_rainfall_data(db_path)
 
-    print(rain_df)
+    # Limit dates
+    rain_df = filter_by_date(rain_df, START_DATE, END_DATE)
 
 if __name__ == "__main__":
     main()
